cfe82701ba3d6e57537703dc6ccaf48e703f443a
lasyagovindwar <165920927+lasyagovindwar@users.noreply.github.com>
2025-03-24 12:38:10 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,10 @@
+class CommandLineDemo2{
+public static void main(String args[]){
+String name=args[0];
+String roll_no=args[1];
+String dept=args[2];
+System.out.println("name"+name);
+System.out.println("roll_no"+roll_no);
+System.out.println("dept"+dept);
+}
+}
