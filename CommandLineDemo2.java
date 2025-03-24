d52389659bca895f5fb762a413785835bd3ad069
lasyagovindwar <165920927+lasyagovindwar@users.noreply.github.com>
2025-03-24 12:45:20 +0530

    Add files via upload

--- CommandLineDemo2.java
+++ CommandLineDemo2.java
@@ -1,10 +1,18 @@
-class CommandLineDemo2{
-public static void main(String args[]){
-String name=(args[0]);
-String roll_no=(args[1]);
-String dept=(args[2]);
-System.out.println("name"  +name);
-System.out.println("roll_no"  +roll_no);
-System.out.println("dept"  +dept);
+class CommandLineDemo2 {
+    public static void main(String args[]) {
+        // Check if enough arguments are provided
+        if (args.length < 3) {
+            System.out.println("Usage: java CommandLineDemo2 <name> <roll_no> <dept>");
+            return;
+        }
+
+        String name = args[0];
+        String roll_no = args[1];
+        String dept = args[2];
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll_no);
+        System.out.println("Department: " + dept);
+    }
 }
-}
+
