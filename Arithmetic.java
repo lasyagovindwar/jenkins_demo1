c162131d71fbb9243da9311b591fe728017dc37d
lasyagovindwar <165920927+lasyagovindwar@users.noreply.github.com>
2025-06-10 12:37:27 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+ class Arithmetic {
+ public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+       
+        int sum = a + b;         // Addition
+        int difference = a - b;  // Subtraction
+        int product = a * b;     // Multiplication
+        int quotient = a / b;    // Division
+        int remainder = a % b;   // Modulus
+
+      
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Addition (a + b): " + sum);
+        System.out.println("Subtraction (a - b): " + difference);
+        System.out.println("Multiplication (a * b): " + product);
+        System.out.println("Division (a / b): " + quotient);
+        System.out.println("Modulus (a % b): " + remainder);
+    }
+}
+
